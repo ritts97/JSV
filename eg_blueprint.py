b9552f06032b076200ef350a4d604bcaeb88c5ee
Ritwika Ghosh <ritwikaghosh48@gmail.com>
2020-09-02 23:03:18 +0530

    Modified request method

--- eg_blueprint.py
+++ eg_blueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, url_for, Response, jsonify
 
 eg_blueprint = Blueprint('eg_blueprint', __name__)
 
-@eg_blueprint.route('/', methods=['GET'])
+@eg_blueprint.route('/', methods=['POST'])
 def fetchdata():
     list = [{"service":"Blood Pressure","price":"20"}, {"service":"Blood Sugar","price":"30"}]
     return jsonify(list)
