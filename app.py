b259b42fedcc5fd4dda02e6dff61874a958477f0
SATANIK <ritwikaghosh48@gmail.com>
2020-09-03 17:29:00 +0530

    Added CORS

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 from eg_blueprint import eg_blueprint
 
 app = Flask(__name__)
+CORS(app,resources={r"/*":{"origins":"*"}})
 app.register_blueprint(eg_blueprint)
 
